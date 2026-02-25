08e6725979ebdc2dfc1257416dc354eacf4ec1cf
tejas kahirnar <tejaskhairnar256@gmail.com>
2026-02-25 11:48:25 +0530

    save changes

--- server/server.js
+++ server/server.js
@@ -43,12 +43,13 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: config.nodeEnv === 'production' 
+  origin: config.nodeEnv === 'production'
     ? [config.frontendUrl, /\.growthvalley\.(in|com)$/]
-    : config.frontendUrl,
+    : true,
+  // : config.frontendUrl,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control', 'Accept', 'Origin'],
 }));
 
 // Body parser middleware
